09c5402d42babc8741440f23dec8b6aed1300968
hiromatu-kengo <fko2647025@stu.o-hara.ac.jp>
2026-05-19 10:49:34 +0900

    260518_boss

--- RE_human/Assets/hiromatu/BossAI.cs
+++ RE_human/Assets/hiromatu/BossAI.cs
@@ -5,14 +5,31 @@ public class BossAI : MonoBehaviour
     enum State
     {
         Idle,
-        Move
+        Move,
+        Chase,
+        MeleeAttack,
+        DashAttack
     }
 
     State currentState;
 
     public float moveSpeed = 3f;
+    public float chaseSpeed = 5f;
+
     public float moveRange = 5f;
     public float idleTime = 2f;
+
+    public float meleeRange = 2f;
+    public float dashRange = 5f;
+
+    public float dashCooldown = 3f;
+
+    bool canDash = true;
+
+    bool isAttacking = false;
+
+    // プレイヤー発見距離
+    public float detectRange = 6f;
 
     float idleTimer;
 
@@ -20,15 +37,42 @@ public class BossAI : MonoBehaviour
 
     Rigidbody2D rb;
 
+    Transform player;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        // Playerタグを探す
+        player = GameObject.FindGameObjectWithTag("Player").transform;
 
         ChangeState(State.Idle);
     }
 
     void Update()
     {
+        if (isAttacking)
+        {
+            return;
+        }
+        // プレイヤーとの距離
+        float playerDistance =
+            Vector2.Distance(transform.position, player.position);
+
+        // 発見したら追尾
+        if (playerDistance < meleeRange)
+        {
+            ChangeState(State.MeleeAttack);
+        }
+        else if (playerDistance < dashRange && canDash)
+        {
+            ChangeState(State.DashAttack);
+        }
+        else if (playerDistance < detectRange)
+        {
+            ChangeState(State.Chase);
+        }
+
         switch (currentState)
         {
             case State.Idle:
@@ -37,6 +81,17 @@ public class BossAI : MonoBehaviour
 
             case State.Move:
                 Move();
+                break;
+
+            case State.Chase:
+                Chase();
+                break;
+            case State.MeleeAttack:
+                MeleeAttack();
+                break;
+
+            case State.DashAttack:
+                DashAttack();
                 break;
         }
     }
@@ -49,11 +104,9 @@ public class BossAI : MonoBehaviour
 
         if (idleTimer <= 0)
         {
-            // Xだけランダム
             float randomX =
                 Random.Range(-moveRange, moveRange);
 
-            // Yは固定
             targetPosition = new Vector2(
                 transform.position.x + randomX,
                 transform.position.y
@@ -65,15 +118,12 @@ public class BossAI : MonoBehaviour
 
     void Move()
     {
-        // 方向
         float direction =
             Mathf.Sign(targetPosition.x - transform.position.x);
 
-        // 横移動のみ
         rb.linearVelocity =
             new Vector2(direction * moveSpeed, 0);
 
-        // 距離判定
         float distance =
             Mathf.Abs(targetPosition.x - transform.position.x);
 
@@ -81,6 +131,15 @@ public class BossAI : MonoBehaviour
         {
             ChangeState(State.Idle);
         }
+    }
+
+    void Chase()
+    {
+        float direction =
+            Mathf.Sign(player.position.x - transform.position.x);
+
+        rb.linearVelocity =
+            new Vector2(direction * chaseSpeed, 0);
     }
 
     void ChangeState(State newState)
@@ -91,5 +150,63 @@ public class BossAI : MonoBehaviour
         {
             idleTimer = idleTime;
         }
+    }
+    void MeleeAttack()
+    {
+        isAttacking = true;
+
+        rb.linearVelocity = Vector2.zero;
+
+        Debug.Log("近接攻撃");
+
+        Invoke(nameof(EndMeleeAttack), 1f);
+    }
+
+    void EndMeleeAttack()
+    {
+        isAttacking = false;
+
+        ChangeState(State.Idle);
+    }
+    void DashAttack()
+    {
+        isAttacking = true;
+
+        canDash = false;
+
+        rb.linearVelocity = Vector2.zero;
+
+        Debug.Log("ため開始");
+
+        Invoke(nameof(StartDash), 1f);
+    }
+
+    void StartDash()
+    {
+        float direction =
+            Mathf.Sign(player.position.x - transform.position.x);
+
+        rb.linearVelocity =
+            new Vector2(direction * 25f, 0);
+
+        Debug.Log("突進！");
+
+        Invoke(nameof(StopDash), 0.5f);
+    }
+
+    void StopDash()
+    {
+        rb.linearVelocity = Vector2.zero;
+
+        isAttacking = false;
+
+        ChangeState(State.Idle);
+
+        Invoke(nameof(ResetDash), dashCooldown);
+    }
+
+    void ResetDash()
+    {
+        canDash = true;
     }
 }
---
+++ RE_human/Assets/hiromatu/PlayerMove.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // A,Dキー or ← →
+        float moveInput = Input.GetAxisRaw("Horizontal");
+
+        // 横移動
+        rb.linearVelocity =
+            new Vector2(moveInput * moveSpeed, 0);
+    }
+}
