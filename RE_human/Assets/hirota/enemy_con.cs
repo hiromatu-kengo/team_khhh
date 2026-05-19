ba8250215f9ab1cfcc28477abe65a38911419503
hirota-05 <fko2647001@stu.o-hara.ac.jp>
2026-05-19 18:29:49 +0900

    player

--- RE_human/Assets/hirota/enemy_con.cs
+++ RE_human/Assets/hirota/enemy_con.cs
@@ -2,36 +2,26 @@ using UnityEngine;
 
 public class enemy_con : MonoBehaviour
 {
-    int hp = 0;
-
+    int hp;
     int maxHp = 5;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         hp = maxHp;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Meleeattack"))
+        if (collision.CompareTag("Meleeattack"))
         {
-            //HPが減る
             hp--;
-            Debug.Log("ss");
+
+            Debug.Log("敵HP : " + hp);
+
+            if (hp <= 0)
+            {
+                Destroy(gameObject);
+            }
         }
-
-        if (hp <= 0)
-        {
-            Destroy(gameObject);
-        }
-
-
     }
 }
