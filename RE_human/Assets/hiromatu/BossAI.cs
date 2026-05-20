cce5fb787e01511749ae24c61efeca745567317c
hiromatu-kengo <fko2647025@stu.o-hara.ac.jp>
2026-05-20 18:49:13 +0900

    2605/20_boss2
    アタック判定と盾を追加して突進攻撃のためを赤くした（エラーが出てるので修正）

--- RE_human/Assets/hiromatu/BossAI.cs
+++ RE_human/Assets/hiromatu/BossAI.cs
@@ -12,6 +12,7 @@ public class BossAI : MonoBehaviour
     }
 
     State currentState;
+    SpriteRenderer spriteRenderer;
 
     public float moveSpeed = 3f;
     public float chaseSpeed = 5f;
@@ -21,6 +22,8 @@ public class BossAI : MonoBehaviour
 
     public float meleeRange = 2f;
     public float dashRange = 5f;
+    public float dashSpeed = 15f;
+    public float dashTime = 0.5f;
 
     public float dashCooldown = 3f;
 
@@ -29,10 +32,25 @@ public class BossAI : MonoBehaviour
 
     bool isAttacking = false;
 
+    //ボスが右を向いているのか
+    bool isFacingRight = true;
+
+    //突進攻撃の方向を固定する
+    float dashDirection;
+
     // プレイヤー発見距離
     public float detectRange = 6f;
 
     float idleTimer;
+
+    //近接攻撃の中心位置
+    public Transform attackPoint;
+
+    //近接攻撃の半径
+    public float attackRadius = 1f;
+
+    //プレイヤーのレイヤー
+    public LayerMask playerLayer;
 
     //Vector2 : 「2Dの位置や方向
     Vector2 targetPosition;
@@ -55,6 +73,7 @@ public class BossAI : MonoBehaviour
 
     void Update()
     {
+        //攻撃中はAIを停止する
         if (isAttacking)
         {
             return;
@@ -63,15 +82,17 @@ public class BossAI : MonoBehaviour
         float playerDistance =
             Vector2.Distance(transform.position, player.position);
 
-        // 発見したら追尾
+        //近距離なら近接攻撃
         if (playerDistance < meleeRange)
         {
             ChangeState(State.MeleeAttack);
         }
+        //中距離なら突進攻撃
         else if (playerDistance < dashRange && canDash)
         {
             ChangeState(State.DashAttack);
         }
+        //プレイヤーを見つけたら追尾
         else if (playerDistance < detectRange)
         {
             ChangeState(State.Chase);
@@ -98,6 +119,7 @@ public class BossAI : MonoBehaviour
                 DashAttack();
                 break;
         }
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Idle()
@@ -144,7 +166,7 @@ public class BossAI : MonoBehaviour
         float distance =
             Mathf.Abs(targetPosition.x - transform.position.x);
 
-        //目標地点にかなり近づいたら
+        //目標地点にかなり近づいたら待機
         //ゲームではピッタリ0にならないので0.1未満にしてる
         if (distance < 0.1f)
         {
@@ -184,57 +206,131 @@ public class BossAI : MonoBehaviour
         //攻撃中の移動を停止する
         rb.linearVelocity = Vector2.zero;
 
+        //攻撃判定を出す
+        Collider2D hitPlayer =
+            Physics2D.OverlapCircle(
+                attackPoint.position,
+                attackRadius,
+                playerLayer
+                );
+
         Debug.Log("近接攻撃");
 
+        if(hitPlayer != null)
+        {
+            Debug.Log("近接攻撃ヒット");
+        }
+
         //一秒後にEndMeleeAttackを実行する
+        //時間差で実行
         Invoke(nameof(EndMeleeAttack), 1f);
     }
 
     void EndMeleeAttack()
     {
+        //攻撃中のフラッグをOFFにする
         isAttacking = false;
 
+        //待機状態へ戻す
         ChangeState(State.Idle);
     }
     void DashAttack()
     {
+        //攻撃中
         isAttacking = true;
 
+        //再突進を禁止する
         canDash = false;
 
+        //一旦停止（攻撃をためている）
         rb.linearVelocity = Vector2.zero;
+
+        //プレイ屋の方向を固定する
+        dashDirection =
+            Mathf.Sign(player.position.x - transform.position.x);
+
+        //赤くする
+        spriteRenderer.color = Color.red;
 
         Debug.Log("ため開始");
 
+        //一秒後に突進開始
         Invoke(nameof(StartDash), 1f);
     }
 
     void StartDash()
     {
-        float direction =
-            Mathf.Sign(player.position.x - transform.position.x);
-
+        //高速で突進する
         rb.linearVelocity =
-            new Vector2(direction * 25f, 0);
+            new Vector2(dashDirection * dashSpeed, 0);
 
         Debug.Log("突進！");
 
-        Invoke(nameof(StopDash), 0.5f);
+        //0.5秒後に停止
+        Invoke(nameof(StopDash), dashTime);
     }
 
     void StopDash()
     {
+        //停止
         rb.linearVelocity = Vector2.zero;
 
+        //白に戻す
+        spriteRenderer.color = Color.white;
+
+        //攻撃中のフラッグを終える
         isAttacking = false;
 
+        //待機状態に戻る
         ChangeState(State.Idle);
 
+        //クールタイム開始
         Invoke(nameof(ResetDash), dashCooldown);
     }
 
     void ResetDash()
     {
+        //突進攻撃の再使用を可能にする
         canDash = true;
+    }
+
+    void FacePlayer()
+    {
+        //プレイヤーが右にいる場合
+        if (player.position.x > transform.position.x)
+        {
+            //右向き
+            transform.localScale = new Vector3(1, 1, 1);
+
+            //右向きの保存
+            isFacingRight = true;
+        }
+        else
+        {
+            //左向き
+            transform.localScale = new Vector3(-1, 1, 1);
+
+            //左向きの保存
+            isFacingRight = false;
+        }
+    }
+    
+    //Sceneビューで当たり判定を表示
+    void OnDrawGizmosSelected()
+    {
+        // attackPointがない場合は終了
+        if (attackPoint == null)
+        {
+            return;
+        }
+
+        // 赤色設定
+        Gizmos.color = Color.red;
+
+        // 攻撃範囲を円で表示
+        Gizmos.DrawWireSphere(
+            attackPoint.position,
+            attackRadius
+        );
     }
 }
