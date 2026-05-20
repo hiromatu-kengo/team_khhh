8ac39cdb22c95332111dada11284dae3580332a8
hirota-05 <fko2647001@stu.o-hara.ac.jp>
2026-05-20 18:04:24 +0900

    player_shield

--- RE_human/Assets/hirota/Player/Player_Shield.cs
+++ RE_human/Assets/hirota/Player/Player_Shield.cs
@@ -12,10 +12,28 @@ public class Player_Shield : MonoBehaviour
     // 出ているシールド
     GameObject currentShield;
 
+    //シールドクールタイム
+    float span = 5f;
+
+    float delta = 5;
+
+    //シールドを使える時間
+    float span1 = 0.5f;
+
+    float delta1 = 0;
+
     void Update()
     {
+
+        this.delta += Time.deltaTime;
+
+        //シールドが出ているなら出現時間タイマーを進める
+        if (currentShield != null)
+        {
+            this.delta1 += Time.deltaTime;
+        }
         // 押している間
-        if (Keyboard.current.wKey.isPressed)
+        if (Keyboard.current.wKey.isPressed && (this.delta > this.span) && (this.delta1 < this.span1))
         {
             // まだ出ていないなら生成
             if (currentShield == null)
@@ -39,6 +57,13 @@ public class Player_Shield : MonoBehaviour
             if (currentShield != null)
             {
                 Destroy(currentShield);
+                
+                //リセット
+                this.delta = 0;
+
+                //リセット
+                this.delta1 = 0;
+
             }
         }
     }
