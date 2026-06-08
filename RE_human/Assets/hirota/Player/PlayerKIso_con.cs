1d1d8b00e59c74724479366e2fbe084d96f8ebd7
hirota-05 <fko2647001@stu.o-hara.ac.jp>
2026-06-08 16:27:13 +0900

    player

--- RE_human/Assets/hirota/Player/PlayerKIso_con.cs
+++ RE_human/Assets/hirota/Player/PlayerKIso_con.cs
@@ -7,6 +7,9 @@ public class player_con : MonoBehaviour
 
     Rigidbody2D rigid2D;
 
+    //アニメーション追加
+    Animator anim;
+
     // 1回目ジャンプ力
     
     [SerializeField] float firstJumpForce = 800.0f;
@@ -46,6 +49,16 @@ public class player_con : MonoBehaviour
     // 他スクリプトからアクセスダッシュ中かどうか
     [HideInInspector] public bool isDashing = false;
 
+    //プレイヤーの死亡判定
+    private bool isDead = false;
+
+    [SerializeField] private float attackMotionTime = 0.3f; // 攻撃アニメーション時間
+    private float attackTimer = 0f; //時間のカウント
+    //攻撃までのクールタイム
+    [SerializeField] private float attackCoolTime = 0.5f;
+    //クールタイムのカウント
+    private float coolTimeTimer = 0f;
+
     private bool _kirikae = false;
 
     public bool kirikae
@@ -62,10 +75,27 @@ public class player_con : MonoBehaviour
         Cursor.visible = true;
 
         Cursor.lockState = CursorLockMode.None;
+
+        this.anim = GetComponent<Animator>();
     }
 
     void Update()
     {
+        //攻撃タイマー
+        if (attackTimer > 0)
+        {
+            attackTimer -= Time.deltaTime;
+        }
+
+        //近接クールタイム
+        if (coolTimeTimer > 0)
+        {
+            coolTimeTimer -= Time.deltaTime;
+        }
+
+
+        //死んでいるなら処理しない
+        if (isDead) return;
         //ボタンを押されたかどうかの処理はこちら
 
         // 最大回数未満ならジャンプ可能
@@ -112,10 +142,12 @@ public class player_con : MonoBehaviour
         //近接攻撃
         if (kirikae)
         {
-            //左クリック
-            if (Mouse.current.leftButton.wasPressedThisFrame)
+            //左クリック& クールタイムが終わっている
+            if (Mouse.current.leftButton.wasPressedThisFrame && coolTimeTimer <= 0)
             {
                 meleeAttack = true;
+                attackTimer = attackMotionTime; // アニメーション維持時間
+                coolTimeTimer = attackCoolTime; // クールタイムセット！
             }
         }
 
@@ -126,10 +158,49 @@ public class player_con : MonoBehaviour
 
             Cursor.visible = !kirikae;
         }
+
+        //アニメーシ
+        if (isDashing)
+        {
+            //ダッシュ中なら最優先ダッシュアニメ
+            anim.Play("Dash");
+        }
+        else if (attackTimer > 0)
+        {
+            //攻撃タイマーが残っているなら最優先
+            anim.Play("Attack");
+        }
+        else if (jumpCount > 0)
+        {
+            //空中にいるときジャンプ
+            anim.Play("Jump");
+        }
+        else
+        {
+            // 地面にいるダッシュ中でない
+            if (move.x != 0)
+            {
+                //移動入力ラン
+                anim.Play("Run");
+            }
+            else
+            {
+                //移動していない待機
+                anim.Play("Idle");
+            }
+        }
     }
 
     private void FixedUpdate()
     {
+
+        //死んでいるなら物理移動、攻撃をしない
+        if (isDead)
+        {
+            rigid2D.linearVelocity = new Vector2(0, rigid2D.linearVelocity.y);
+            return;
+        }
+
         if (!isDashing)
         {
             // 横移動
@@ -155,8 +226,9 @@ public class player_con : MonoBehaviour
             jump = "";
         }
 
+     
         //近接攻撃
-        if(meleeAttack)
+        if (meleeAttack)
         {
             //プレイヤーの向きを判定
             float direction = Mathf.Sign(transform.localScale.x);
@@ -171,15 +243,41 @@ public class player_con : MonoBehaviour
             meleeAttack = false;
         }
 
-
-
+        
 
 
     }
 
+    // HPが0時に呼び出す
+    void Die()
+    {
+        if (isDead) return; // 二重に呼び出されるのを防ぐ
+
+        isDead = true;
+
+        // 死亡アニメーション
+        anim.Play("Death");
+
+        //プレイヤーを画面から完全に消去
+        Invoke("DestroyPlayer", 1.5f);
+    }
+
+    //消滅
+    void DestroyPlayer()
+    {
+        Destroy(gameObject);
+       //ゲームオーバー画面
+
+    }
+
+
+
     // 当たり判定
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
+        if (collision.otherCollider.gameObject != this.gameObject) return;
+
         // Groundタグならジャンプ回数をリセット
         if (collision.gameObject.CompareTag("Ground"))
         {
@@ -206,7 +304,7 @@ public class player_con : MonoBehaviour
         //HPがなくなったら消える
         if (playerHp <= 0)
         {
-            Destroy(gameObject);
+            Die();
         }
     }
         private void OnTriggerEnter2D(Collider2D collision)
@@ -221,7 +319,7 @@ public class player_con : MonoBehaviour
 
         if (playerHp <= 0)
         {
-            Destroy(gameObject);
+           Die();
         }
     }
 
