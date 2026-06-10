5bdc2204b8bbd59fded2f995de45e780c58ced7d
Ryo2911 <fko2647010@stu.o-hara.ac.jp>
2026-06-10 15:54:17 +0900

    scene移行

--- RE_human/Assets/hiraki/StageIkou.cs
+++ RE_human/Assets/hiraki/StageIkou.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StageIkou : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Boss1Room");
+        }
     }
 }
