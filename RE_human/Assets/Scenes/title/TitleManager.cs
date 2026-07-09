d425fdfde501fbea9a9418ef287fc12dbe62b3cc
hiromatu-kengo <fko2647025@stu.o-hara.ac.jp>
2026-07-09 17:30:23 +0900

    タイトルがかっこよくなりました

--- RE_human/Assets/Scenes/title/TitleManager.cs
+++ RE_human/Assets/Scenes/title/TitleManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using Unity.VisualScripting;
+using NUnit.Framework;
 
 
 
@@ -13,6 +15,35 @@ public class TitleManager : MonoBehaviour
     // インスペクターから、ゲーム本編のシーン名を指定できるようにする
     [Header("--- 移動先のシーン名 ---")]
     public string gameSceneName = "Stage1";
+
+    [Header("点滅させるボタンのCanvasGroup")]
+    public CanvasGroup startButtonCanvas;
+    public CanvasGroup exitButtonCanvas;
+
+    [Header("点滅の設定")]
+    [Tooltip("点滅の周期（秒）")]
+    public float blinkSpeed = 2.0f;
+
+    //[Range(0f, 1f)]
+    [Tooltip("点滅の最小透明度")]
+    public float minAlpha = 0.3f;
+
+    private void Update()
+    {
+        //ゆっくり点滅させる計算
+        float wave = (Mathf.Sin(Time.time * blinkSpeed) + 1.0f) / 2.0f; // 0～1の値を作る
+        //設定した一番薄い透明度からくっきりの間を滑らかに変化させる
+        float currentAlpha = Mathf.Lerp(minAlpha, 1.0f, wave);
+        //それぞれのボタンの透明度を舞フレーム更新する
+        if(startButtonCanvas != null)
+        {
+            startButtonCanvas.alpha = currentAlpha;
+        }
+        if(exitButtonCanvas != null)
+        {
+            exitButtonCanvas.alpha = currentAlpha;
+        }
+    }
 
     /// <summary>
     /// スタートボタンが押されたときに実行する関数
