c3cdda319cba60bacf46cce7b24bfeb15ad41f01
kubotananato <fko2647003@stu.o-hara.ac.jp>
2026-07-23 17:36:31 +0900

    a

--- RE_human/Assets/hiraki/StageIkou2.cs
+++ RE_human/Assets/hiraki/StageIkou2.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StageIkou1 : MonoBehaviour
+public class StageIkou2 : MonoBehaviour
 {
 
     private void OnCollisionEnter2D(Collision2D collision)
